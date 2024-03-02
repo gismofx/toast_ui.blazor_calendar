9432c778b4584694a57344ce4abba03a5360fc96
Kevin Krüger <kevin22042001@gmail.com>
2024-03-02 20:49:06 +0100

    ColorJsonConverter added

---
+++ toast_ui.blazor_calendar/Services/JsonConverters/ColorJsonConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace toast_ui.blazor_calendar.Services.JsonConverters
+{
+    /// <summary>
+    /// Creates a JsonConverter for the Color class.
+    /// </summary>
+    public class ColorJsonConverter : JsonConverter<Color>
+    {
+        public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            string hexValue = reader.GetString();
+
+            // Remove the hash sign, if present
+            if (hexValue != null && hexValue.StartsWith("#"))
+                hexValue = hexValue.Substring(1);
+
+            return ColorTranslator.FromHtml("#" + hexValue);
+        }
+
+        public override void Write(Utf8JsonWriter writer, Color value, JsonSerializerOptions options)
+        {
+            string hexValue = ColorTranslator.ToHtml(value);
+            writer.WriteStringValue(hexValue);
+        }
+    }
+}
